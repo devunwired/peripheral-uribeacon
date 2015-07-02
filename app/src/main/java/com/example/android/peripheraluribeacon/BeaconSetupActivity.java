78c2f32368f59803f9f17c972295f0148cc4e463
Dave Smith <smith@wiresareobsolete.com>
2015-07-02 14:57:12 -0600

    Initial commit.

---
+++ app/src/main/java/com/example/android/peripheraluribeacon/BeaconSetupActivity.java
@@ -0,0 +1,96 @@
+package com.example.android.peripheraluribeacon;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class BeaconSetupActivity extends Activity implements
+        View.OnClickListener {
+    private static final String TAG =
+            BeaconSetupActivity.class.getSimpleName();
+
+
+    /* UI to control advertise value */
+    private EditText mUrlText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+
+        mUrlText = (EditText) findViewById(R.id.text_url);
+
+        findViewById(R.id.button_start).setOnClickListener(this);
+        findViewById(R.id.button_stop).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkBluetoothStatus();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent serviceIntent = new Intent(this, UriBeaconService.class);
+        switch (v.getId()) {
+            case R.id.button_start:
+                Uri uri = Uri.parse(mUrlText.getText().toString());
+                serviceIntent.setData(uri);
+                startService(serviceIntent);
+                break;
+            case R.id.button_stop:
+                stopService(serviceIntent);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown view id");
+        }
+    }
+
+    private boolean checkBluetoothStatus() {
+        BluetoothManager manager =
+                (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        BluetoothAdapter adapter = manager.getAdapter();
+        /*
+         * We need to enforce that Bluetooth is first enabled, and take the
+         * user to settings to enable it if they have not done so.
+         */
+        if (adapter == null || !adapter.isEnabled()) {
+            //Bluetooth is disabled
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBtIntent);
+            finish();
+            return false;
+        }
+
+        /*
+         * Check for Bluetooth LE Support.  In production, our manifest entry will keep this
+         * from installing on these devices, but this will allow test devices or other
+         * sideloads to report whether or not the feature exists.
+         */
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "No LE Support.", Toast.LENGTH_SHORT).show();
+            finish();
+            return false;
+        }
+
+        /*
+         * Check for advertising support. Not all devices are enabled to advertise
+         * Bluetooth LE data.
+         */
+        if (!adapter.isMultipleAdvertisementSupported()) {
+            Toast.makeText(this, "No Advertising Support.", Toast.LENGTH_SHORT).show();
+            finish();
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/android/peripheraluribeacon/UriBeaconService.java
@@ -0,0 +1,154 @@
+package com.example.android.peripheraluribeacon;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.IBinder;
+import android.os.ParcelUuid;
+import android.util.Log;
+
+import org.uribeacon.beacon.UriBeacon;
+
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+
+public class UriBeaconService extends Service {
+    private static final String TAG = UriBeaconService.class.getSimpleName();
+
+    private static final int NOTIFICATION_ID = 42;
+
+    /* Full Bluetooth UUID that defines the URI Service */
+    public static final ParcelUuid URI_SERVICE =
+            ParcelUuid.fromString("0000fed8-0000-1000-8000-00805f9b34fb");
+
+    private BluetoothLeAdvertiser mBluetoothLeAdvertiser;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        BluetoothManager manager =
+                (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = manager.getAdapter();
+        mBluetoothLeAdvertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
+
+        startForeground(NOTIFICATION_ID, buildForegroundNotification());
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Uri uri = intent.getData();
+        if (uri == null) {
+            Log.w(TAG, "Intent received with null Uri");
+            stopSelf();
+        } else {
+            restartAdvertising(uri);
+        }
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopForeground(true);
+
+        stopAdvertising();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void startAdvertising(Uri uri) {
+        if (mBluetoothLeAdvertiser == null) return;
+
+        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                .setConnectable(false)
+                .setTimeout(0)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+                .build();
+
+        try {
+            AdvertiseData data = new AdvertiseData.Builder()
+                    .setIncludeDeviceName(false)
+                    .setIncludeTxPowerLevel(false)
+                    .addServiceUuid(URI_SERVICE)
+                    .addServiceData(URI_SERVICE, buildDataPacket(uri))
+                    .build();
+
+            mBluetoothLeAdvertiser
+                    .startAdvertising(settings, data, mAdvertiseCallback);
+        } catch (URISyntaxException e) {
+            Log.w(TAG, "Unable to start advertising", e);
+        }
+    }
+
+    private void stopAdvertising() {
+        if (mBluetoothLeAdvertiser == null) return;
+
+        mBluetoothLeAdvertiser.stopAdvertising(mAdvertiseCallback);
+    }
+
+    private void restartAdvertising(Uri uri) {
+        stopAdvertising();
+        startAdvertising(uri);
+    }
+
+    private Notification buildForegroundNotification() {
+        Intent notificationIntent = new Intent(this, BeaconSetupActivity.class);
+        PendingIntent trigger = PendingIntent
+                .getActivity(this, 0, notificationIntent, 0);
+
+        return new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Broadcasting URL")
+                .setContentText("Broadcasting URL")
+                .setContentIntent(trigger)
+                .build();
+    }
+
+    private byte[] buildDataPacket(Uri uri) throws URISyntaxException {
+        //We cannot read the TX power from Android. This uses a sane default.
+        UriBeacon model = new UriBeacon.Builder()
+                .uriString(uri.toString())
+                .build();
+        byte[] uriBytes = model.getUriBytes();
+        //Flags + Power + URI
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1 + 1 + uriBytes.length);
+        buffer.put(model.getFlags());
+        buffer.put(model.getTxPowerLevel());
+        buffer.put(uriBytes);
+
+        return byteBufferToArray(buffer);
+    }
+
+    private static byte[] byteBufferToArray(ByteBuffer bb) {
+        byte[] bytes = new byte[bb.position()];
+        bb.rewind();
+        bb.get(bytes, 0, bytes.length);
+        return bytes;
+    }
+
+    private AdvertiseCallback mAdvertiseCallback = new AdvertiseCallback() {
+        @Override
+        public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+            Log.i(TAG, "LE Advertise Started.");
+        }
+
+        @Override
+        public void onStartFailure(int errorCode) {
+            Log.w(TAG, "LE Advertise Failed: "+errorCode);
+        }
+    };
+}
